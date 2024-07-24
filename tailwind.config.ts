b186350a1fb3a7d26319f398c71192df2d445436
Ezedin <ezedin@roi.ad>
2024-07-24 14:16:56 -0700

    ft: add color palates.

---
+++ tailwind.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      'sm': '643px',
+      'md': '1220px',
+      'lg': '1920px',
+      'xl': '2000px',
+      '2xl': '2500px',
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "primaryGreen": "#32C0CC",
+        "secondaryGreen": "#70DCAE",
+        "lightGreen": "#E3FAFF",
+        "extraLightGreen": "#F1FFF9",
+        "primaryDark": "#282D41",
+        "secondaryDark": "#5A5F73",
+        "primaryWhite": "#FFFFFF",
+        "lightGrayBg": "#F5F5F5",
+        "lighterGrayBg": "#F8F8F8",
+        "lightestGray": "#BFC0C6",
+        "lighterGray": "#E9E9E9",
+        "checkboxGray": "#DDDEE3",
+        "backgroundGray": "#F5F7FA",
+        "footerGray": "#72757F",
+        "lightPink": "#FFF4FA",
+        "brightPink": "#EA78A1",
+        "lightBlue": "#E3FAFF",
+        "brightBlue": "#50BCEBB3",
+        "brightYellow": "#F6D66F",
+        "lightYellow": "#FBF2DD",
+        "extraLightYellow": "#FFF9EB"
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
